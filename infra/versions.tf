85b99ddee7017e6198ba307df6f3ce6c264b4f85
CiceroBezerra2025 <cicero.bezerra@aluno.impacta.edu.br>
2026-02-08 04:40:57 +0000

    Fix: mesclando alterações 2

--- infra/versions.tf
+++ infra/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Ajuste conforme sua necessidade
+      version = "~> 6.0" # Ajuste conforme sua necessidade
     }
   }
 }
